6e93b67df77348f4d5c6ed60016504bb1497c91c
person020 <tint0n69964@gmail.com>
2025-06-25 11:54:12 -0600

    v2.1

--- config.js
+++ config.js
@@ -1,2 +1,30 @@
-// This file is retired : settings now can be tailored using url section in configuration.txt, when needed
-//
+var config = {
+  title: "Infinity SMP",
+  center: [0, 64, 0], // adjust to your spawn if needed
+  zoom: 1,
+  world: "infinitySMP",
+  worldname: "infinitySMP",
+  map: "surface",
+  controls: {
+    pan: true,
+    zoom: true,
+    compass: true,
+    mapselector: true,
+    chatbox: false,
+    link: true,
+    coordsbox: true,
+    config: false
+  },
+  worlds: {
+    "infinitySMP": "Infinity SMP"
+  },
+  tilesets: [
+    {
+      name: "surface",
+      path: "tiles/infinitySMP/surface",
+      zoomlevels: 5,
+      tileSize: 128,
+      mapzoomout: 3
+    }
+  ]
+};
